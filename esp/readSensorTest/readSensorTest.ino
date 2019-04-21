a168943053719b15ec78098cefd8f29f3e02af1a
Vito Cudemo <vcudemo@me.com>
2019-04-21 09:46:13 +0200

    Sensors read and push to ThingSpeak done

--- esp/readSensorTest/readSensorTest.ino
+++ esp/readSensorTest/readSensorTest.ino
@@ -1,4 +1,4 @@
-#include <DHT.h>
+#include <DHTesp.h>
 #include <ESP8266WiFi.h>
 #include <time.h>
 #include "ThingSpeak.h"
@@ -17,20 +17,85 @@ const char *myWriteAPIKey = SECRET_WRITE_APIKEY;
 // Humidity & Temperature
 #define DHTPIN 0
 #define DHTTYPE DHT11
-DHT dht(DHTPIN, DHTTYPE);
+DHTesp dht;
 
 // Moisture
+// http://wiki.seeedstudio.com/Grove-Moisture_Sensor/
 int MOISTURE = A0;
+#define MOISTURE_PIN A0
 
 // Light
-int LIGHT = 2;
+#define LIGHT_PIN 2
 
-float h = 0;
-float t = 0;
+float humidity = 0;
+float temperature = 0;
+
+String myStatus = "";
 
 void setup()
 {
   Serial.begin(115200);
+
+  // Setup DHTesp for Temperature and Humidity Sensor
+  dht.setup(DHTPIN, DHTesp::DHTTYPE);
+
+  // Set timezone
+  const int timezone = 0;
+  const int dst_off = 0;
+  configTime(timezone * 3600, dst_off, "pool.ntp.org", "time.nist.gov");
+  // wait for time() being adjusted, as a side effect of configTime
+  while (time(NULL) < 28800 * 2)
+  {
+    delay(500);
+  }
+
+  connectWifi();
+
+  ThingSpeak.begin(client); // Initialize ThingSpeak
+}
+void loop()
+{
+
+  humidity = dht.getHumidity();
+  temperature = dht.getTemperature();
+  printToConsole();
+  updateThinkSpeak();
+
+  delay(10000);
+}
+
+int getMoistureSensorData()
+{
+  return analogRead(MOISTURE_PIN);
+}
+
+int getLightSensorData()
+{
+  return analogRead(LIGHT_PIN);
+}
+
+void printToConsole()
+{
+  // Check for error
+  if (isnan(humidity) || isnan(temperature))
+  {
+    return;
+  }
+
+  Serial.print("Moisture = ");
+  Serial.println(getMoistureSensorData());
+  Serial.print("Light = ");
+  Serial.println(getLightSensorData());
+  Serial.print("Temperature = ");
+  Serial.println(temperature);
+  Serial.print("Humidity = ");
+  Serial.println(humidity);
+
+  delay(1000);
+}
+
+void connectWifi()
+{
 
   WiFi.mode(WIFI_STA);
   if (WiFi.status() != WL_CONNECTED)
@@ -46,47 +111,33 @@ void setup()
     Serial.print("Connected, IP = ");
     Serial.println(WiFi.localIP());
   }
-
-  const int timezone = 0;
-  const int dst_off = 0;
-  configTime(timezone * 3600, dst_off, "pool.ntp.org", "time.nist.gov");
-  // wait for time() being adjusted, as a side effect of configTime
-  while (time(NULL) < 28800 * 2)
-  {
-    delay(500);
-  }
-}
-void loop()
-{
-
-  h = dht.readHumidity();
-  t = dht.readTemperature();
-  printToConsole();
 }
 
-int getMoistureSensorData()
+void updateThinkSpeak()
 {
-  return analogRead(MOISTURE);
-}
 
-int getLightSensorData()
-{
-}
-
-void printToConsole()
-{
   // Check for error
-  if (isnan(h) || isnan(t))
+  if (isnan(humidity) || isnan(temperature))
   {
     return;
   }
+  // set the fields with the values
+  ThingSpeak.setField(1, getMoistureSensorData());
+  ThingSpeak.setField(2, getLightSensorData());
+  ThingSpeak.setField(3, temperature);
+  ThingSpeak.setField(4, humidity);
 
-  Serial.print("Humidity = ");
-  Serial.println(h, 3);
-  Serial.print("Temperature = ");
-  Serial.println(t, 3);
+  // set the status
+  ThingSpeak.setStatus(myStatus);
 
-  Serial.print("Moisture = ");
-  Serial.println(getMoistureSensorData());
-  delay(1000);
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+
+  if (x == 200)
+  {
+    Serial.println("Channel update successful.");
+  }
+  else
+  {
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
 }
