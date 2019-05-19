a03919289701be433d66bcb28baa30d175a7e020
Mischa Imbiscuso <mischa.imbiscuso@gmail.com>
2019-05-19 10:27:37 +0200

    fixed error in spring boot

--- connected-plants-rest/src/main/java/ch/fhnw/iot/connectedPlants/raspberry/PlantApplication/web/ConnectedPlantsRestController.java
+++ connected-plants-rest/src/main/java/ch/fhnw/iot/connectedPlants/raspberry/PlantApplication/web/ConnectedPlantsRestController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/connected-plants")
 @CrossOrigin
-public class ConnectedPlantsController {
+public class ConnectedPlantsRestController {
 
     @Autowired
     private ConnectedPlantsRepository connectedPlantRepository;
